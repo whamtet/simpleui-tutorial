a0487099b032a331ce1757ead42a2541c8e79734
Matthew Molloy <whamtet@gmail.com>
2023-11-02 07:36:23 +0900

    horay for sci!

--- src/simpleui/app.cljs
+++ src/simpleui/app.cljs
@@ -1,13 +1,19 @@
 (ns simpleui.app
 	(:require
-		[promesa.core :as p]))
+		[promesa.core :as p]
+		[sci.core :as sci]))
 
 (def Router (aget (js/require "itty-router") "Router"))
 (def router (Router.))
 
+(defn pr-str2 [x]
+	(if (string? x)
+		x
+		(pr-str x)))
+
 (.post router "/"
 			(fn [req]
 				(p/let [body (.text req)]
-					(js/Response. body))))
+					(-> body sci/eval-string pr-str2 js/Response.))))
 
 (def ^:export fetch (.-handle router))
