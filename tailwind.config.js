4793f88f0d3ca8dd1be3eb24ac1b761aad9e4abd
Matthew Molloy <whamtet@gmail.com>
2023-11-04 12:08:48 +0900

    frontend hello world

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: [
+    "frontend/*.html",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
