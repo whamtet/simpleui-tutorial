869cb0a6a8beff7710ceb1b0acaed174bb58262f
Matthew Molloy <whamtet@gmail.com>
2023-11-05 09:11:09 +0900

    intro!

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+const colors = require('tailwindcss/colors')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ["./frontend/*.html"],
 	theme: {
 		extend: {},
+		colors: {
+			...colors,
+			'clj-green': '#A2DA5F',
+			'clj-blue': '#97B3F8'
+		}
 	},
 	plugins: [],
 }
