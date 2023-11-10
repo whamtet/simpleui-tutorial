c2398a202e9dee682fde15545c84ba3493311430
Matthew Molloy <whamtet@gmail.com>
2023-11-10 14:15:29 +0900

    better formatter

--- format.cljs
+++ format.cljs
@@ -4,18 +4,17 @@
 		["fs" :as fs]
 		["highlight.js/lib/common$default" :as hljs]))
 
-(def slurp-lines #(-> % fs/readFileSync str .trim (.split "\n")))
-(defn spit-lines [f s]
-	(->> s (string/join "\n") (fs/writeFileSync f)))
+(def slurp #(-> % fs/readFileSync str))
+(def spit fs/writeFileSync)
 
-(def get-code #(second (re-find #"<code[^>]*>([^<]+)" %)))
+(defn map-rest [f [x & items]]
+	(cons x (map f items)))
 
-(defn format-code [line]
-	(if-let [code (get-code line)]
-		(->> code hljs/highlightAuto .-value (.replace line code))
-		line))
+(defn format-head [chunk]
+	(let [[code] (.split chunk "</code>")]
+		(->> code hljs/highlightAuto .-value (.replace chunk code))))
 
-(->> "index.template.html"
-		 slurp-lines
-		 (map format-code)
-		 (spit-lines "frontend/index.html"))
+(->> (.split (slurp "index.template.html") "/*hl*/ ")
+		 (map-rest format-head)
+		 string/join
+		 (spit "frontend/index.html"))
