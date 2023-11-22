f405c0710b17a8ba4b7cdd448687cb9fd2f467ec
Matthew Molloy <whamtet@gmail.com>
2023-11-23 03:30:41 +0900

    nice trigger, nice

--- frontend/editors.js
+++ frontend/editors.js
@@ -2,5 +2,10 @@ const editors = {};
 for (const el of document.querySelectorAll('.editor')) {
 	const editor = ace.edit(el.id);
 	editor.session.setMode('ace/mode/clojure');
+	if (el.hasAttribute('watch')) {
+		const trigger = document.getElementById(el.id.replace('editor', 'tests') + '-run');
+		editor.session.on('change', () => trigger.click())
+	}
+
 	editors[el.id] = editor;
 }
